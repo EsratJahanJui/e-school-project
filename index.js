daed65fe9590737dad7d4ddc457374c687ba68bb
EsratJahanJui <esratjahanjui327@gmail.com>
2024-02-05 23:27:24 +0600

    commit message

---
+++ index.js
@@ -0,0 +1,9 @@
+// const navSlide = () => {
+//     const burger = document.querySelector('.nav-bar');
+//     const nav = document.querySelector('.nav-flex');
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+// });
+// }
+
+// navSlide();
